6e150bcee9c9f975585418a520b245b2431daaa8
Sebastien Cagnon <cagnonsebastien@gmail.com>
2015-10-07 15:10:22 +0900

    setup files

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ virtualenv env
 # using pip from the new virtualenv
 # define target directory in the app with -t or --target
 # IMPORTANT: use --no-compile option, because .pyc created on your machine will probably work on your robot.
-./env/bin/pip install -r requirements.txt --target WikipediaExample/lib --no-compile
+./env/bin/pip install -r requirements.txt --target PepperForm/lib --no-compile
--- setup_win.sh
+++ setup_win.sh
@@ -5,4 +5,4 @@ virtualenv env
 # using pip from the new virtualenv
 # define target directory in the app with -t or --target
 # IMPORTANT: use --no-compile option, because .pyc created on your machine will probably work on your robot.
-pip.exe install -r requirements.txt --target WikipediaExample/lib --no-compile
+pip.exe install -r requirements.txt --target PepperForm/lib --no-compile
