78d014ebbce9dfc19ee9b2bd935e7cd2143b6f29
Sebastien Cagnon <cagnonsebastien@gmail.com>
2015-10-08 16:36:11 +0900

    working survey app using only dialog & python service

--- PepperForm/lib/GoogleFormService.py
+++ PepperForm/lib/GoogleFormService.py
@@ -9,7 +9,7 @@ class GFService(object):
 	"""Naoqi service to use Google Form for survey on Pepper"""
 
 	# concept names
-	CONCEPT_QUESTION = "question"
+	MKEY_QUESTION = "GoogleForm/Question"
 	CONCEPT_CHOICES  = "choices"
 
 	@qi.nobind
@@ -21,25 +21,27 @@ class GFService(object):
 	@qi.bind(qi.Void, paramsType=(qi.String,))
 	def loadForm(self, url):
 		"""Initialize a new GFParser object with url"""
+		print "loading form"
 		self.url = url
 		self.form = PyGoogleForm.GFParser(url)
 		self.questions = self.form.getQuestionIDs()
 		self.questionIndex = -1
 
-	@qi.bind(qi.Void, paramsType=(qi.Void,))
+	@qi.bind(qi.Void)
 	def submit(self):
 		"""Submit and unloads the form"""
 		self.form.submit()
 		self.url = ""
 
-	@qi.bind(qi.String, paramsType=(qi.Void,))
+	@qi.bind(qi.String)
 	def getFormURL(self):
 		"""Return the url of the current form, or empty string is no form is loaded"""
 		return self.url
 
-	@qi.bind(qi.String, paramsType=(qi.Void,))
+	@qi.bind(qi.String)
 	def nextQuestion(self):
 		"""Loads next question and sets up dynamic concepts. Returns question type or 'finished'"""
+		print "nextQuestion"
 		self.questionIndex += 1
 		if self.questionIndex >= len(self.questions):
 			return "finished"
@@ -47,15 +49,18 @@ class GFService(object):
 		if self.questionInfo[1] == "ss-checkbox":
 			self.checked =[]
 		# Dialog settings
-		self["ALDialog"].setConcept(self.CONCEPT_QUESTION, self.getLanguageNU(), self.questionInfo[2])
+		print "nextQuestion 2"
+		self["ALMemory"].raiseEvent(self.MKEY_QUESTION, self.questionInfo[2])
 		if isinstance(self.questionInfo[3], list):
-			self["ALDialog"].setConcept(self.CONCEPT_CHOICES, self.getLanguageNU(), self.questionInfo[3])
+			self["ALDialog"].setConcept(self.CONCEPT_CHOICES, self.getLanguageNU(), self.questionInfo[3], _async=True)
+		print "nextQuestion 3"
 		# return question type
 		return self.questionInfo[1]
 
 	@qi.bind(qi.Void, paramsType=(qi.String,))
 	def answer(self, answer):
 		"""answers the question"""
+		print "answer: ", answer
 		self.form.answerQuestion(self.questionInfo[0], answer)
 
 	@qi.bind(qi.Void, paramsType=(qi.String,))
@@ -63,10 +68,12 @@ class GFService(object):
 		"""Adds answer to the list of checked questions"""
 		self.checked.append(answer)
 
-	@qi.bind(qi.Void, paramsType=(qi.Void,))
+	@qi.bind(qi.Void)
 	def answerCheckbox(self):
 		"""Answers the checkbox question with list of checks"""
-		self.form.answerQuestion(self.questionInfo[0], self.answer)
+		print "here"
+		self.form.answerQuestion(self.questionInfo[0], self.checked)
+		print "there"
 
 	@qi.nobind
 	def __getitem__(self, serviceName):
@@ -87,8 +94,11 @@ def main():
 	app = qi.Application(sys.argv)
 	app.start()
 
-	amService = AdvancedMediaService(app.session)
-	id = app.session.registerService("AdvancedMedia", amService)
+	gfService = GFService(app.session)
+	id = app.session.registerService("GoogleForm", gfService)
 
 	app.run()
 	sys.exit()
+
+if __name__ == '__main__':
+	main()
